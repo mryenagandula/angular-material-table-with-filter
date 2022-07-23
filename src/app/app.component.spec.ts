173b6667c1388900c7302b1adc11d4d6046c0f30
SpringSrikanth <srikanthyenagagandula89@gmail.com>
2022-07-23 16:54:34 +0530

    Updated project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-material-table'`, () => {
+  it(`should have as title 'angular-material-table-with-filter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-material-table');
+    expect(app.title).toEqual('angular-material-table-with-filter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-material-table app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-material-table-with-filter app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-material-table';
+  title = 'angular-material-table-with-filter';
 }
